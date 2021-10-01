4fa2787d9866ca821ded52d17de2afb7d701cbf8
sonntagsgesicht <sonntagsgesicht@icloud.com>
2021-10-01 22:16:05 +0000

    updated git_tools (created by auxilium)

--- auxilium/tools/git_tools.py
+++ auxilium/tools/git_tools.py
@@ -108,8 +108,8 @@ def push_git(remote='None', path=getcwd()):
 
     out, err = BytesIO(), BytesIO()
     porcelain.push(Repo(path), remote, BRANCH, out, err)
-    for line in out.readlines():
+    for line in out.buffer.readlines():
         log(INFO, ICONS[""] + line)
-    for line in err.readlines():
+    for line in err.buffer.readlines():
         log(INFO, ICONS[""] + line)
     return 0
